a9381447c57e06b08f4dfdccaec83a1e7ebe9d5f
Joshua Morris <php.rack@gmail.com>
2011-03-23 18:32:45 -0400

    Adding favicons, one for admin/public each.

--- templates/admin.html.php
+++ templates/admin.html.php
@@ -2,6 +2,7 @@
 <head>
 	<title>Sandbox Admin</title>
 	<link rel="stylesheet" href="/public/styles.css" />
+	<link rel="icon" type="image/png" href="/public/adminfav.png" />
 </head>
 <body>
 	<h1>Admin site (logged in as '<?php echo Prack_Utils::singleton()->escapeHTML( $user ); ?>')</h1>
--- templates/public.html.php
+++ templates/public.html.php
@@ -2,6 +2,7 @@
 <head>
 	<title>Sandbox Public site</title>
 	<link rel="stylesheet" href="/public/styles.css" />
+	<link rel="icon" type="image/png" href="/public/publicfav.png" />
 </head>
 <body>
 	<h1>Public site.</h1>
