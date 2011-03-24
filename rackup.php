5f6350f547bb16fd1e9bc6b200e2bf6fa8ad0b0d
Joshua Morris <php.rack@gmail.com>
2011-03-24 01:12:04 -0400

    Tidying up rackup.php with Prack_Config.

--- rackup.php
+++ rackup.php
@@ -4,23 +4,27 @@ include "lib/php-rack/autoload.php";
 include "lib/php-rb/autoload.php";
 include "lib/sandbox/autoload.php";
 
-// Configuration to simplify code below:
+// Middleware app callbacks:
 
-$auth_callback = create_function(
-	'$username,$password',
-	'return ( $username->raw() == "superadmin" && $password->raw() == "secret" );'
-);
+function onConfigure( $env )
+{
+	$env->set( 'sandbox.root'     , Prb::Str( dirname( __FILE__ ) ) );
+	$env->set( 'sandbox.templates', Prb::Str( join( DIRECTORY_SEPARATOR, array( $env->get( 'sandbox.root' )->raw(), 'templates' ) ) ) );
+}
 
-$static_urls = Prb::Ary( array( Prb::Str( '/public' ) ) );
-$templates   = Prb::Str( join( DIRECTORY_SEPARATOR, array( dirname( __FILE__ ), 'templates' ) ) );
-
-$env = Prack_ModPHP_Compat::singleton()->extractEnv( $_SERVER );
-$env->set( 'sandbox.root'     , Prb::Str( dirname( __FILE__ ) ) );
-$env->set( 'sandbox.templates', Prb::Str( join( DIRECTORY_SEPARATOR, array( $env->get( 'sandbox.root' )->raw(), 'templates' ) ) ) );
+function onAuthBasicAuthenticate( $username, $password )
+{
+	return $username->raw() == 'superadmin' && $password->raw() == 'secret';
+}
 
 // Building the domain:
 
+$static_urls = Prb::Ary( array( Prb::Str( '/public' ) ) );
+
 $domain = Prack_Builder::domain()
+
+  ->using( 'Prack_Config'         )->withArgs( 'onConfigure' )->build()
+  ->using( 'Prack_Runtime'        )->withArgs( 'Total' )->build()
   ->using( 'Prack_ShowExceptions' )->build()
   ->using( 'Prack_Etag'           )->build()
   ->using( 'Prack_ContentType'    )->withArgs( Prb::Str( 'text/html' )                     )->build()
@@ -33,7 +37,7 @@ $domain = Prack_Builder::domain()
     ->run( new Sandbox_Public() )
 
   ->map( '/admin' )
-    ->using( 'Prack_Auth_Basic'    )->withArgs( Prb::Str( 'sandbox admin site' ), $auth_callback )->build()
+    ->using( 'Prack_Auth_Basic'    )->withArgs( Prb::Str( 'sandbox admin site' ), 'onAuthBasicAuthenticate' )->build()
     ->using( 'Sandbox_Showenv'     )->build()
     ->using( 'Sandbox_Showruntime' )->build()
     ->using( 'Prack_Runtime'       )->build()
@@ -44,8 +48,9 @@ $domain = Prack_Builder::domain()
 
 ->toMiddlewareApp();
 
-// Rendering the response:
+// Handling the response:
 
-Prack_ModPHP_Compat::singleton()->render( $domain->call( $env ) );
-
-?>
+$modphp_compat = Prack_ModPHP_Compat::singleton();
+$modphp_compat->render(
+  $domain->call( $modphp_compat->extractEnv( $_SERVER ) )
+);
